cea656193d8423d01eaed19b45b222a07b3ea462
Tati92-L <77683219+Tati92-L@users.noreply.github.com>
2022-05-05 00:22:54 +0300

    trying to solve problem with rollbar

--- src/init.jsx
+++ src/init.jsx
@@ -20,13 +20,15 @@ export default async (socket) => {
   filter.add(filter.getDictionary('en'));
 
   const rollbarConfig = {
-    accessToken: '59e25b3382c34f3b92ce4aed5ae7ea14',
+    accessToken: `${process.env.ROLLBAR_ACCESS_TOKEN}`,
     captureUncaught: true,
     captureUnhandledRejections: true,
     payload: {
       environment: 'production',
     },
   };
+
+  // `${process.env.ROLLBAR_ACCESS_TOKEN}`,
 
   return (
     <RollbarProvider config={rollbarConfig}>
